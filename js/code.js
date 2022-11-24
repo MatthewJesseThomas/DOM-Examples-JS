b362f57eec9e19137d6624ea15a04525f01a8bb6
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-24 10:57:28 +0200

    'main'

---
+++ js/code.js
@@ -0,0 +1,23 @@
+//=====DOM Manipulation with addEventListener======//
+
+//=====1st Approach=====//
+// let button = document.getElementById('button');
+
+// button.addEventListener('click', (event) => {
+//         document.getElementById("para1").innerHTML = '<h1 style="color: crimson">Hello Dying World</h1>';
+//         document.getElementById("para2").innerText = '<h1 style="color: lightsteel">Hello New World</h1>';
+
+// })
+
+//=====2nd Approach=====//
+// let btnDisplay = document.querySelector('#button');
+
+// btnDisplay.addEventListener('click',()=>{
+//     document.write('Oops IH8U-Virus_Executing...')
+// })
+
+// let output = document.getElementById('output');
+// output.innerHTML = '<h1 style="color: crimson">Come Die With Us</h1>';
+// console.log(output);
+// document.write('Hello,World');
+console.log(alert("I Am Death"));
